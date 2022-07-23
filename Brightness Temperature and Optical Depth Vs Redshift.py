ac9b9c9fa2ff603124f380a8fb89c0f6ba746e66
SUBHAM PATRA <63844019+patrasubham@users.noreply.github.com>
2022-07-23 16:38:44 +0530

    Update Brightness Temperature and Optical Depth Vs Redshift.py

--- Brightness Temperature and Optical Depth Vs Redshift.py
+++ Brightness Temperature and Optical Depth Vs Redshift.py
@@ -1,3 +1,5 @@
+#Brightness Temperature and Optical Depth Vs Redshift(z)
+
 from math import sin
 from numpy import array,arange
 import numpy as np
@@ -9,6 +11,7 @@ for T_s in [1,10,100]:
     d=[]
     z=6.0
     for i in range (len(z1)):
+        #Formulae used to calculate optical depth and Brightness Temperature 
         T_nu=(4.0/T_s)*(0.044*(6.626*10**(-34.0))*0.7/0.2)*((0.3*(1+z)**3.0)+0.69+((8.4*10**-5.0)*(1+z)**4.0))**(1.0/2.0)
         delta=(T_s-(2.73*(1+z)))*T_nu/(1+z)
         T.append(T_nu)
@@ -22,14 +25,14 @@ for T_s in [1,10,100]:
     plt.grid()
     plt.xlabel('z')
     plt.ylabel('\u03C4_\u03C5')
-    plt.title('\u03C4_\u03C5 vs z')
+    plt.title('Optical Depth vs Redshift(z)')
     plt.figure(2)
     plt.plot(Z,d,label=T_s)
     plt.legend({'T_s=1','T_s=10','T_s=100'})
     plt.grid()
     plt.xlabel('z')
-    plt.ylabel('\u03B4T_b')
-    plt.title('\u03B4T_b vs z')
+    plt.ylabel('T_b')
+    plt.title('Brightness Teperature vs Redshift(z)')
 plt.show()
     
 
